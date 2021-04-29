f4dae03269bcd9883c2f083a50bd01d631ba04ad
Ivanp <ivanpinavva@gmail.com>
2021-04-29 13:54:58 +0200

    primer commit

---
+++ src/app/contador/contador.module.ts
@@ -0,0 +1,15 @@
+
+import { NgModule } from "@angular/core";
+import { ContadorComponent } from "./contador/contador.component";
+
+
+
+
+@NgModule({
+    declarations: [
+        ContadorComponent
+    ]
+    
+})
+
+export class ContadorModule {}
